83f1164ed5d8c52c08146bc682db102552322fbb
Casres <christian@casillas.tech>
2022-07-19 14:56:08 -0400

    Adding all files

---
+++ manatee.js
@@ -0,0 +1,34 @@
+// const buttonEl = document.getElementById('my-button');
+
+// const clickHandler = function() {
+
+//     let count = parseInt(this.getAttribute('data-count'));
+//     count++
+
+//     this.setAttribute('data-count', count);
+//     this.textContent = `Clicks: ${count}`;
+
+//     console.log('btn clicked');
+// };
+
+// buttonEl.addEventListener('click', clickHandler);
+
+const buttonEl = document.getElementById('my-button');
+
+const clickHandler = function () {
+    let count = 0;
+    
+    if (count >= 5 ) {
+        return this.textContent = `potatoes: ${count}`
+    }
+
+    return function () {
+        count++;
+        this.textContent = `hips: ${count}`;
+        
+        console.log('btn is clicked, commander');
+    }
+};
+
+buttonEl.addEventListener('click', clickHandler()); 
+
